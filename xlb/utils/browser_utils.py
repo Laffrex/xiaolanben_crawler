4ad4f094a15b9a407b13764f134e8237f0d70c8a
sx_zhuozb <sx_zhuozb@knownsec.com>
2025-03-19 17:07:11 +0800

    兼容3.8+，解决驱动问题

--- xlb/utils/browser_utils.py
+++ xlb/utils/browser_utils.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 
@@ -58,7 +59,18 @@ def init_driver():
     
     try:
         driver_path = get_driver_path()
-        return webdriver.Chrome(executable_path=driver_path, options=options)
+        
+        # 根据Python版本使用不同的初始化方式
+        if sys.version_info >= (3, 9):
+            # Python 3.9+ 使用 Service 类
+            from selenium.webdriver.chrome.service import Service
+            service = Service(driver_path)
+            driver = webdriver.Chrome(service=service, options=options)
+        else:
+            # Python 3.8 及以下版本使用 executable_path
+            driver = webdriver.Chrome(executable_path=driver_path, options=options)
+            
+        return driver
     except WebDriverException as e:
         if "version" in str(e).lower():
             raise WebDriverException(
